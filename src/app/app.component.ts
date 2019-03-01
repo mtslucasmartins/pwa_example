ecbb3c6238f63795fd63a938ff5249f85522dc0a
Lucas Martins <dev.lucasmartins@gmail.com>
2019-03-01 15:24:37 -0300

    changes to package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { SwPush } from '@angular/service-worker';
+import { Component, OnInit } from '@angular/core';
+import { SwPush, SwUpdate } from '@angular/service-worker';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pwa-zapcontabil';
+export class AppComponent implements OnInit {
+  title = environment.production;
 
   readonly VAPID_PUBLIC_KEY = 'BLSKBIHrsFCeLUO3FwI95mfSubQiZlno-CTZPDBBoTH6P4CQ-SnEZtlBNM-TWRlk-u3Q36JdjLLk69WYNWJ2rOw';
 
@@ -16,8 +17,9 @@ export class AppComponent {
   //   "privateKey": "d-FafnJ0zkCN3zH0Vvz9arsvCX15oMk8WmyJyBjWFM0"
   // };
 
-  constructor(
-    private swPush: SwPush) { }
+  constructor(private swUpdate: SwUpdate,
+              private swPush: SwPush) {
+  }
 
   public subscribeToNotifications(): void {
 
@@ -28,4 +30,21 @@ export class AppComponent {
       .catch(err => console.error('Could not subscribe to notifications', err));
   }
 
+  public ngOnInit() {
+
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+
+        if (confirm("New version available. Load New Version?")) {
+          window.location.reload();
+        }
+
+      });
+
+    }
+
+
+
+  }
 }
