d8b59f10a47886c2a4ad5984ac3ff681126fd83e
Lucas Martins <dev.lucasmartins@gmail.com>
2019-03-01 14:48:41 -0300

    changes to package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-zapcontabil';
+
+  readonly VAPID_PUBLIC_KEY = 'BLSKBIHrsFCeLUO3FwI95mfSubQiZlno-CTZPDBBoTH6P4CQ-SnEZtlBNM-TWRlk-u3Q36JdjLLk69WYNWJ2rOw';
+
+  // keys = {
+  //   "publicKey": "BLSKBIHrsFCeLUO3FwI95mfSubQiZlno-CTZPDBBoTH6P4CQ-SnEZtlBNM-TWRlk-u3Q36JdjLLk69WYNWJ2rOw",
+  //   "privateKey": "d-FafnJ0zkCN3zH0Vvz9arsvCX15oMk8WmyJyBjWFM0"
+  // };
+
+  constructor(
+    private swPush: SwPush) { }
+
+  public subscribeToNotifications(): void {
+
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then((subscription) => console.log(subscription))
+      .catch(err => console.error('Could not subscribe to notifications', err));
+  }
+
 }
